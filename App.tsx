a407e786d0a5c35449895cdd3559aa46f4acc205
Nicolasjdpc <nicolasjdpc@gmail.com>
2021-10-06 19:52:19 -0500

    Update App.tsx
    Test Nicolas Jimenez

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 /*
-izack
+Nicolas
 */
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
